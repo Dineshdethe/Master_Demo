6ae85f4992acddb6638b769d3c52a7db303447b1
Dineshdethe <Dinesh174@>
2024-11-06 18:03:55 +0530

    print added

---
+++ src/main/java/com/app/Test.java
@@ -0,0 +1,10 @@
+package com.app;
+
+public class Test {
+
+    public void m1(){
+        System.out.println("Hiii github");
+        System.out.println("Hey gitss");
+        System.out.println("Hiiiiiiiiiiii");
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import com.app.Test;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Test t1=new Test();
+        t1.m1();
+    }
+}
